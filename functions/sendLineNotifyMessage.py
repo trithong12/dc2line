ace5046e0b45b5b25dd6231ef2386ff8aa7ec365
TonyChen <TonyChen@DESKTOP-GMOFK63>
2021-07-15 16:31:44 +0800

    first commit

---
+++ functions/sendLineNotifyMessage.py
@@ -0,0 +1,16 @@
+import requests
+
+def sendLineNotifyMessage(token, msg):
+
+    headers = {
+        "Authorization": "Bearer " + token, 
+        "Content-Type" : "application/x-www-form-urlencoded"
+    }
+
+    payload = {
+        "message": msg
+    }
+    
+    r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=payload)
+    
+    return r.status_code
---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+
+# from flask import Flask
+
+import discord
+from discord.ext import commands
+
+from functions.sendLineNotifyMessage import sendLineNotifyMessage
+
+# app = Flask(__name__)
+
+bot = commands.Bot(command_prefix='>t')
+
+lineNotifyToken = os.environ["LINE_NOTIFY_TOKEN"]
+discordBotToken = os.environ["DISCORD_BOT_TOKEN"]
+
+@bot.listen()
+async def on_voice_state_update(member, before, after):
+    if member.bot:
+        return
+    if not member or not after.channel:
+        return
+
+    message = "%s 剛進入群組《%s》語音頻道《%s》" % (member.name, member.guild, after.channel.name)
+    sendLineNotifyMessage(lineNotifyToken, message)
+
+try:
+    print("Running")
+    bot.run(discordBotToken)    
+except KeyboardInterrupt:
+    print("Exiting")
+    bot.logout()
+    bot.close()
+finally:
+    bot.logout()
+    bot.close()
+    print("Disconnected")
