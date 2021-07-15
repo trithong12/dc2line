86675222a13f4d6ffa056c7b76c8670b6cf815db
TonyChen <TonyChen@DESKTOP-GMOFK63>
2021-07-15 17:08:09 +0800

    fix EOF problem

--- main.py
+++ main.py
@@ -24,6 +24,5 @@ async def on_voice_state_update(member, before, after):
     message = "%s 剛進入群組《%s》語音頻道《%s》" % (member.name, member.guild, after.channel.name)
     sendLineNotifyMessage(lineNotifyToken, message)
 
-try:
-    print("Running")
-    bot.run(discordBotToken)
+print("Running")
+bot.run(discordBotToken)
